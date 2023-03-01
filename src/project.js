cd14502cf1ad30e7738c55240cf0bc1e4768cf51
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-03-01 21:02:56 +0300

    Write code for project module

---
+++ src/project.js
@@ -0,0 +1,10 @@
+import task from "./task.js";
+
+export default Project = (newName) => {
+    this.name = newName;
+    this.tasks = [];
+
+    const getUnfinishedTasks = () => {
+        //Write code here
+    }
+}
