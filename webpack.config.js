696b241526167882021785fedaf87cd01002c329
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-03-01 20:38:24 +0300

    Add style and image files to src dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
